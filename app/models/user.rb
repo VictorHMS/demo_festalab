fc93fdd35ffbfeb690b8d0b9144fac315448ac6b
VictorHMS <vhms@cin.ufpe.br>
2023-10-12 00:05:11 -0300

    subindo projeto

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+    validates :email,
+    format: { with: URI::MailTo::EMAIL_REGEXP, message: "inválido"  },
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 4, maximum: 254 }
+    
+    validates :cpf,
+    format: { with: CPF_PATTERN, message: "inválido"}
+
+    validates :phone,
+    format: { with: PHONE_PATTERN, message: "inválido"}
+end
